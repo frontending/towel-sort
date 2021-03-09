532ca659d0615ae5649dc94285df8c1aa2490435
frontending <mymage259@gmail.com>
2021-03-09 20:03:10 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix === undefined || matrix[0] === undefined) {
+    return []
+  }
+  let arr = matrix.map(function(el, index) {
+    if (index % 2 !== 0) {
+      el.reverse()
+    }
+    return el
+  })
+  return arr.join(',').split(',')
 }
